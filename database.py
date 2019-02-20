32d17d59e1ccf74d05fa8f8b39695861933a489f
Chan Woo <14cpark@gmail.com>
2019-02-19 21:57:31 -0800

    create database and collection, test insert works

--- database.py
+++ database.py
@@ -3,5 +3,8 @@ from pymongo import MongoClient
 
 def createDatabase():
 	client = MongoClient()
-	mydb = client["worddatabase"]
-	print(client.list_database_names())
+	database = client["worddatabase"]
+	collection = database["wordcollection"]
+	
+	mydict = { "Roman": [1, 2, 3, 4] }
+	x = collection.insert_one(mydict)
