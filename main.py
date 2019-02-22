e851f48b56549abe93d0c4c3bc2145a0d07d628e
Chan Woo <14cpark@gmail.com>
2019-02-21 17:57:58 -0800

    moved create_index out of main function correct import of fileIO.py in main.py detail resulting print statements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import sys
 import os
 import json
-import result_generate
+import fileIO
 import bs4
 import re
 
@@ -15,89 +15,89 @@ def tokenize(y) -> list:
         token.extend(l)
     return token
 
+def create_index() -> dict:
+	rootdir = 'webpages\\WEBPAGES_RAW'
+	result = dict()
+	num_doc = 0
+
+	with open('bookkeeping.json', 'r') as f:
+			j_dict = json.load(f)
+
+	for k, v in j_dict.items():
+			if int(k[0]) < 1:
+		
+					num_doc += 1
+					y = []
+					
+					fol, fil = k.split('/')
+					
+					try:
+							file = open(rootdir + "\\" + fol + "\\" + fil, 'r', encoding = 'utf-8')
+
+							soup = bs4.BeautifulSoup(file, 'lxml')
+							soup.prettify()
+							a_txt = soup.find_all('p')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in a_txt])
+
+							b_txt = soup.find_all('a')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
+
+							b_txt = soup.find_all('b')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
+
+							b_txt = soup.find_all('h1')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
+
+							b_txt = soup.find_all('h2')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
+
+
+							b_txt = soup.find_all('h3')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
+
+							b_txt = soup.find_all('body')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
+							
+							b_txt = soup.find_all('title')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
+							
+							b_txt = soup.find_all('strong')
+							y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
+							
+							
+					except:
+							with open(rootdir + "\\" + fol + "\\" + fil, 'r', encoding = 'utf-8') as d:
+									file_str = d.read().replace('\n', '')
+							#print(file_str)
+							y.append(file_str)
+							
+					finally:
+							l = tokenize(y)
+							print(str(fol)+'/'+ str(fil))
+
+							for i in l:
+									freq = l.count(i)
+
+									if i not in result:
+											result[i] = []
+									if (fol, fil, freq) not in result[i]:
+										result[i].append((fol,fil, freq))
+			else:
+				break
+	return (result, num_doc)
+
+	
 if __name__ == '__main__':
+        index, num_doc = create_index()
+        fileIO.write_index_to_file(index)
         
-        rootdir = 'testwebpages'
-        result = dict()
-        num_doc = 0
-        num_uniq = 0
-
-        with open('bookkeeping.json', 'r') as f:
-                j_dict = json.load(f)
-
-        for k, v in j_dict.items():
-                if int(k[0]) < 1:
-            
-                        num_doc += 1
-                        y = []
-                        
-                        fol, fil = k.split('/')
-                        
-                        try:
-                                file = open(rootdir + "\\" + fol + "\\" + fil, 'r', encoding = 'utf-8')
-
-                                soup = bs4.BeautifulSoup(file, 'lxml')
-                                soup.prettify()
-                                a_txt = soup.find_all('p')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in a_txt])
-
-                                b_txt = soup.find_all('a')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-                                b_txt = soup.find_all('b')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-                                b_txt = soup.find_all('h1')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-                                b_txt = soup.find_all('h2')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-
-                                b_txt = soup.find_all('h3')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-                                b_txt = soup.find_all('body')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-                                
-                                b_txt = soup.find_all('title')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-                                
-                                b_txt = soup.find_all('strong')
-                                y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-                                
-                                
-                        except:
-                                with open(rootdir + "\\" + fol + "\\" + fil, 'r', encoding = 'utf-8') as d:
-                                        file_str = d.read().replace('\n', '')
-                                #print(file_str)
-                                y.append(file_str)
-                                
-                        finally:
-                                l = tokenize(y)
-                                print(str(fol)+'/'+ str(fil))
-
-                                for i in l:
-                                        freq = l.count(i)
-
-                                        if i not in result:
-                                                result[i] = []
-                                        if (fol, fil, freq) not in result[i]:
-                                            result[i].append((fol,fil, freq))
-                else:
-                    break
-                
-
-                            
-        result_generate.write_index_to_file(result)
-        
-        print(num_doc)
-        num_uniq = len(result)
-        print(num_uniq)
-        print(os.path.getsize("final.txt"))
+        print('Number of documents:', num_doc)
+        num_uniq = len(index)
+        print('Number of uniques:', num_uniq)
+        print('Size of index on file:', os.path.getsize("final.txt"), 'bytes')
 
     
-        info_list = result["mondego"]
+        info_list = index["mondego"]
         print("URLs for word: mondego")
         print(len(info_list))
         for i in info_list:
