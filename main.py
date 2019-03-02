94d7e1f2bbdd2d5c580b1d5e76d12dffb805f6d9
Yeonjae Hong <yeonjh@uci.edu>
2019-03-02 04:38:22 -0800

    Used tokenize function instead of libraries

--- main.py
+++ main.py
@@ -5,15 +5,35 @@ import json
 import fileIO
 import bs4
 import re
+from multiprocessing import Pool
+from collections import defaultdict
 
-def tokenize(y) -> list:
-    token = []
-    for text in y:
-        text = text.rstrip()
-        p = re.compile("[a-z]+") #pattern for token
-        l = p.findall(text.lower()) #make a list of words that fit pattern
-        token.extend(l)
-    return token
+#rootdir = 'webpages'
+rootdir = 'testwebpages'
+
+def tokenize(f):
+    #pattern to ensure that word is only alphanumeric
+    pattern = re.compile('[a-zA-Z]+')
+    frequencies = defaultdict(int)
+    tokens = []
+    #going through all the lines in file
+    for line in f:
+        #making sure that all the characters in line are lowercase
+        new_l = line.lower()
+        
+        #check which words fit the pattern and
+        #using findall to combine into a list
+        l = pattern.findall(new_l)
+
+        #adding the elements of list l into actual list of tokens
+        tokens.extend(l)
+
+    #traversing through the list of tokens and adding to dictionary
+    for i in tokens:
+        #increment the word count by 1
+        frequencies[i] += 1
+
+    return tokens, frequencies
     
     
 def search(index, searchterm) -> list:
@@ -23,71 +43,44 @@ def search(index, searchterm) -> list:
 
     
 def create_index() -> (dict, int):
-    rootdir = 'webpages\\WEBPAGES_RAW'
+
     result = dict()
     num_doc = 0
 
-    with open('bookkeeping.json', 'r') as f:
+
+    with open(rootdir + '\\' + 'bookkeeping.json', 'r') as f:
             j_dict = json.load(f)
 
     for k, v in j_dict.items():
+        token = []
+        freq_ = defaultdict()
+        if int(k[0]) < 1:
             num_doc += 1
             y = []
             
             fol, fil = k.split('/')
             
             try:
-                    file = open(rootdir + "\\" + fol + "\\" + fil, 'r', encoding = 'utf-8')
-
-                    soup = bs4.BeautifulSoup(file, 'lxml')
-                    soup.prettify()
-                    a_txt = soup.find_all('p')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in a_txt])
-
-                    b_txt = soup.find_all('a')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-                    b_txt = soup.find_all('b')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-                    b_txt = soup.find_all('h1')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-                    b_txt = soup.find_all('h2')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-
-                    b_txt = soup.find_all('h3')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-
-                    b_txt = soup.find_all('body')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-                    
-                    b_txt = soup.find_all('title')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-                    
-                    b_txt = soup.find_all('strong')
-                    y.extend([re.sub(r'<.+?>',r'',str(a)) for a in b_txt])
-                    
+                    file = open(rootdir + '\\' + fol + "\\" + fil, 'r', encoding = 'utf-8')
+                    token , freq_ = tokenize(file)
                     
             except:
-                    with open(rootdir + "\\" + fol + "\\" + fil, 'r', encoding = 'utf-8') as d:
-                            file_str = d.read().replace('\n', '')
-                    #print(file_str)
-                    y.append(file_str)
+                    continue
                     
             finally:
-                    l = tokenize(y)
+                    
                     print(str(fol)+'/'+ str(fil))
 
-                    for i in l:
-                            freq = l.count(i)
+                    for i in token:
+                            freq = freq_[i]
 
                             if i not in result:
                                     result[i] = []
                             if (fol, fil, freq) not in result[i]:
                                 result[i].append((fol,fil, freq))
-  
+        else:
+               break;
+            
             
     return (result, num_doc)
 
@@ -100,6 +93,7 @@ if __name__ == '__main__':
             print('Reading index from file...')
             index = fileIO.read_index_from_file()
             num_doc = fileIO.read_num_doc_from_file()
+            num_uniq = len(index)
         else:
             print('Creating index...')
             index, num_doc = create_index()
@@ -115,10 +109,36 @@ if __name__ == '__main__':
         print('Number of uniques:', num_uniq)
         print('Size of index on file:', os.path.getsize("final.txt"), 'bytes')
 
-        searchresult = search(index, 'slide')
+        with open(rootdir + '\\'+ 'bookkeeping.json', 'r') as f:
+                j_dict = json.load(f)
+
+        count = 0
+        searchresult = search(index, 'mondego')
+        mondego = open('mondego.txt', 'w')
+        for i in searchresult:
+            count += 1
+            if (count < 21):
+                mondego.write(j_dict[str(i[0]) + '/' + str(i[1])])
+                print(j_dict[str(i[0]) + '/' + str(i[1])])
+        mondego.close()
+
+        count = 0;
+        searchresult = search(index, 'informatics')
+        in4 = open('informatics.txt', 'w')
+        for i in searchresult:
+            count += 1
+            if (count < 21):
+                in4.write(j_dict[str(i[0]) + '/' + str(i[1])])
+                print(j_dict[str(i[0]) + '/' + str(i[1])])
+        in4.close()
+
+        count = 0;
+        searchresult = search(index, 'irvine')
+        irvine = open('irvine.txt', 'w')
+        for i in searchresult:
+            count += 1
+            if (count < 21):
+                irvine.write(j_dict[str(i[0]) + '/' + str(i[1])])
+                print(j_dict[str(i[0]) + '/' + str(i[1])])
+        irvine.close()
             
-        # info_list = index["mondego"]
-        # print("URLs for word: mondego")
-        # print(len(info_list))
-        # for i in info_list:
-            # print(j_dict[str(i[0]) + '/' + str(i[1])])
